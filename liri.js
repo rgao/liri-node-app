968fd4bb855337e1bbc278122149bdefb19b4ee8
rgao <xinghoshi@gmail.com>
2018-10-31 05:33:55 -0700

    concert-this code implemented

--- liri.js
+++ liri.js
@@ -1,3 +1,20 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+var request = require("request");
+// var spotify = new Spotify(keys.spotify);
+
+if (process.argv[2] === "concert-this") {
+    request("https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp", function(error, response, body) {
+
+        if (!error & response.statusCode === 200) {
+            var result = JSON.parse(body);
+
+            for (i = 0; i < result.length; i++) {
+                console.log("Venue name and location:")
+                console.log(result[i].venue);
+                console.log("Date: " + moment(result[i].datetime).format('MM-DD-YYYY'));
+            }
+        }
+    })
+}
