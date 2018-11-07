eb602a1b5932e72964b01d6826e031209cd4419e
rgao <xinghoshi@gmail.com>
2018-11-06 18:30:23 -0800

    most functionalities

--- liri.js
+++ liri.js
@@ -2,19 +2,47 @@ require("dotenv").config();
 
 var moment = require("moment");
 var request = require("request");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+    id: "a726ed5ef45346b184714406e6ee4ffc",
+    secret: "8b6801a7ed2742a3b08ada7ae8a7ede4"
+});
 
 if (process.argv[2] === "concert-this") {
-    request("https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp", function(error, response, body) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp";
+    request(queryURL, function (error, response, body) {
 
         if (!error & response.statusCode === 200) {
             var result = JSON.parse(body);
 
             for (i = 0; i < result.length; i++) {
-                console.log("Venue name and location:")
-                console.log(result[i].venue);
+                console.log("Venue name and location:", result[i].venue);
                 console.log("Date: " + moment(result[i].datetime).format('MM-DD-YYYY'));
             }
         }
     })
+}
+
+//differences in syntax from above?
+if (process.argv[2] === "spotify-this") {
+    var queryURL = 'https://api.spotify.com/v1/' + process.argv[3] + spotify.id;
+    spotify.request(queryURL)
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (err) {
+        console.error('Error occurred: ' + err);
+    })
+}
+
+if (process.argv[2] === "movie-this") {
+    var queryURL = "http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy";
+    request(queryURL, function(error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
 }
